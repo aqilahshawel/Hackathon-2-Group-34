c574846f01fa56a28985f854e2fad8a1fe34aec2
ainasofeya <117916285+ainasofeya@users.noreply.github.com>
2023-06-28 20:54:51 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,216 +1,138 @@
 #include "LinkedList.h"
 #include "LinkedList.cpp"
-#include "totalAnswer.h"
-#include "playerInfo.h"
+#include "Game.h"
+#include "ResultGame.h"
 #include <iostream>
 #include <string>
 #include <cstdlib>
 #include <fstream>
 using namespace std;
 
+const int NUMSCENES = 7;
 
-void menu();
+//Function prototypes
+void displayScene(int);   
 
 int main()
 {
-	LinkedList<int> list;
-    string line;
-    int answer1, answer2, answer3, answer4, answer5, answer6, answer7;
+  //Define a LinkedList object
+  LinkedList<int> list;
+  
+  //Creating 'game' as instance of ResultGame
+  ResultGame game(&list);
     
-    playerInfo player;
+  //display game intro
+  game.start();
     
-	top:
-	//get player information
-	player.getInfo();
-	
-	
-	fstream input;
-	
-	//**************************
-	//Reading and writing scene1
-	//**************************
-	
-	input.open("scene1.txt");  
-
-    if (input.fail()) {
-        cout << "Failed to open the file." << endl;
-        return 1;
+  //get playerInfo
+  game.getPlayerInfo();
+  
+  top:
+  //start display scene
+  for (int i = 0; i < NUMSCENES; i++)
+  {
+    int answer;
+    if (i==1)
+    {
+        displayScene(i);
+        cin>>answer;
+        //implement dynamic linked list to append nodes
+        list.append(answer);    
+                
+        if (answer== 1)
+        {
+            cout<<endl;
+            game.print("Peter pulled out a paper, you won! :) \n",50);
+            game.print("Peter gives you his money to pay for your lunch. YEAYYY!\n" ,50);
+        }
+        else if (answer == 2)
+        {
+          cout<<endl;
+            game.print("Peter pulled out a paper, you lost. :( \n",50);
+            game.print("You have to pay for your lunch using your own pocketmoney. HUHUHU ;(( \n", 50);
+        }
+        else if (answer == 3)
+        {
+            cout<<endl;
+            game.print("Peter pulled out a paper, it's a draw. :) \n",50);
+            game.print("Peter decided to pay for your lunch anyways as a kind gesture. YEAYYY!\n", 50);
+        }
     }
-
-
-    while (getline(input, line)) {
-        
-		cout << line << endl;
-    }
-
-    input.close();
-    
-    cin>>answer1;
-
-    list.append(answer1);
-    
-    //**************************
-	//Reading and writing scene2
-	//**************************
-	input.open("scene2.txt");  
-
-    if (input.fail()){
-        cout << "Failed to open the file." << endl;
-        return 1;
-    }
-
-
-    while (getline(input, line)) {
-        
-		cout << line << endl;
-    }
-
-    input.close();
-    
-    cin>>answer2;
-
-    list.append(answer2);
-    
-    //**************************
-	//Reading and writing scene3
-	//**************************
-	input.open("scene3.txt");  
-
-    if (input.fail()) {
-        cout << "Failed to open the file." << endl;
-        return 1;
-    }
-
-
-    while (getline(input, line)) {
-        cout << line << endl;
-    }
-
-    input.close();
-    
-    cin>>answer3;
-
-    list.append(answer3);
-    
-    //**************************
-	//Reading and writing scene4
-	//**************************
-	input.open("scene4.txt");  
-
-    if (input.fail()) {
-        cout << "Failed to open the file." << endl;
-        return 1;
-    }
-
-
-    while (getline(input, line)) {
-        
-		cout << line << endl;
-    }
-
-    input.close();
-    
-    cin>>answer4;
-
-    list.append(answer4);
-    
-    //**************************
-	//Reading and writing scene5
-	//**************************
-	input.open("scene5.txt");  
-
-    if (input.fail()) {
-        cout << "Failed to open the file." << endl;
-        return 1;
-    }
-
-
-    while (getline(input, line)) {
-        
-		cout << line << endl;
-    }
-
-    input.close();
-    
-    cin>>answer5;
-
-    list.append(answer5);
-    
-    //**************************
-	//Reading and writing scene6
-	//**************************
-	input.open("scene6.txt");  
-
-    if (input.fail()) {
-        cout << "Failed to open the file." << endl;
-        return 1;
-    }
-
-
-    while (getline(input, line)) {
-        
-		cout << line << endl;
-    }
-
-    input.close();
-    
-    cin>>answer6;
-
-    list.append(answer6);
-    
-    //**************************
-	//Reading and writing scene1
-	//**************************
-	input.open("scene7.txt");  
-
-    if (input.fail()) {
-        cout << "Failed to open the file." << endl;
-        return 1;
-    }
-
-
-    while (getline(input, line)) {
-       
-		cout << line << endl;
-    }
-
-    input.close();
-    
-    cin>>answer7;
-
-    list.append(answer7);
-    
-
-    // Creating 'total' as instance of TotalAnswer with the LinkedList
-    TotalAnswer<int> total(&list);
+    else 
+    {
+        displayScene(i);
+        cin>>answer;
+        //implement dynamic linked list to append nodes
+        list.append(answer);     
+    }     
+  }
+  
+    //display playerInfo
+    game.displayInfo(); 
 
     // Checking the result
-    total.checkPersonality();
+    game.checkPersonality();
+    
+    //closing
+    game.end();
+
+	//to clear the linked list, and start new linkedlist for new round of game
+        list.clear();
+        //reinitializes the playerAnswers list as an empty list. 
+    //This ensures that the list is completely cleared and ready to store answers from the new player. 
+        list=LinkedList<int>();
  
-	//Checking to know the player want to start again or exit
-	int choice;
-	menu();
-	cin>>choice;
-	if (choice == 1)
-		goto top;
-	
-	else if (choice == 2)
-		return 0;
-	
-	else
-	{
-		cout<<"Wrong choice. Please choose only either 1 or 2. Enter your choice again:";
-	    cin>>choice;
-	}
-	   
+    //Checking to know the player want to start again or exit
+    int choice;
+    cin >> choice;
+    
+    while (choice !=3)
+    {
+        if (choice == 1)
+       {
+          goto top;
+       }
+       else if (choice == 2)
+       {
+          game.getPlayerInfo();
+          goto top;
+     }
+       else if (choice == 3)
+       {
+          return 0;
+       }
+       else
+       {
+          cout << "Wrong choice. Please choose either 1 or 2. Enter your choice again: ";
+          cin >> choice;
+        }
+    }
+     
     return 0;
 }
 
-void menu()
+
+//implement reading textfiles
+void displayScene(int sceneIndex)
 {
-	cout << "Thank you for playing this game! From the storyline, it is actually a Personality Quiz to determine what type of flower you are.\n";
-	cout << "Hope you are having fun and enjoy this game :D "<<endl;
-    cout << "1. Restart again the game" <<endl;
-    cout << "2. Exit the game."<<endl;
-    cout << "Please enter your choice."<<endl;
+  string sceneFiles[NUMSCENES] = {"scenario1.txt", "scenario2.txt", "scenario3.txt", "scenario4.txt", "scenario5.txt", "scenario6.txt", "scenario7.txt"};
+    fstream input(sceneFiles[sceneIndex]);
+    
+    if (input.fail())
+    {
+        cout << "Failed to open the file: " << sceneFiles[sceneIndex] << endl;
+        return;
+    }
+
+    string line;
+    while (getline(input, line))
+    {
+      LinkedList<int> list;
+      ResultGame game(&list);
+      //call function print typewriting to display content in textfiles with effect typing
+      game.print(line, 50);
+      cout<<endl;
+    }
+
+    input.close();
 }
