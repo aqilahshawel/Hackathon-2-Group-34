856652a83cfaa083ddd738c2e21b52a9a63ddd14
ainasofeya <117916285+ainasofeya@users.noreply.github.com>
2023-06-28 20:56:05 +0800

    Update LinkedList.cpp

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -11,53 +11,58 @@ using namespace std;
 template <class T>
 void LinkedList<T>:: append(T newAnswer) 
 {
-    ListNode<T>* newNode;
-    ListNode<T>* nodePtr;
+    ListNode* newNode;
+    ListNode* nodePtr;
         
-    newNode = new ListNode<T>(newAnswer);
+  newNode = new ListNode;
     newNode->answer = newAnswer;
     newNode->next = nullptr;
 
-   if (head == nullptr) 
-   {
-      head = newNode;
-      tail = newNode;
-    } 
-	else 
-    {
-    	tail->next = newNode;
-    	tail = newNode;
-//        nodePtr = head;
-//            
-//        while (nodePtr->next != nullptr) 
-//		{
-//            nodePtr = nodePtr->next;
-//        }
-//            nodePtr->next = newNode;
+    if (head == nullptr) 
+  {
+        head = newNode;
     }
-        
+  else 
+  {
+        nodePtr = head;
+            
+        while (nodePtr->next != nullptr)
+    {
+            nodePtr = nodePtr->next;
+        }
+        nodePtr->next = newNode;
+    }
 }
 
 //**************************************************
-// The traverseAndSum function traverse the list and 
-// sum all answer in the node
+// The countChoice function traverse the list and 
+// count each total choice to determine which choice is the highest
 //**************************************************
 
 template <class T>
-int LinkedList<T>:: getSum()
+void LinkedList<T>:: countChoices(T& totalChoice1, T& totalChoice2, T& totalChoice3)
 {
-	ListNode<T>* nodePtr;
-	
-	nodePtr = head;
-	
-	int sum = 0;
-	
-	while (nodePtr != nullptr)
-	{
-		sum+=nodePtr->answer;
-		nodePtr = nodePtr->next;
-	}
-	return sum;
+    ListNode* nodePtr = head;
+    totalChoice1 = totalChoice2 = totalChoice3 = 0;
+
+    while (nodePtr != nullptr) 
+  {
+        switch (nodePtr->answer) 
+    {
+            case 1:
+                totalChoice1++;
+                break;
+            case 2:
+                totalChoice2++;
+                break;
+            case 3:
+                totalChoice3++;
+                break;
+            default:
+                break;
+        }
+        nodePtr = nodePtr->next;
+    }
 }
 
 
@@ -67,10 +72,10 @@ int LinkedList<T>:: getSum()
 //**************************************************
 
 template <class T>
-LinkedList<T>::~LinkedList()
+void LinkedList<T>::clear()
 {
- ListNode<T> *nodePtr; // To traverse the list
- ListNode<T> *nextNode; // To point to the next node
+ ListNode *nodePtr; // To traverse the list
+ ListNode *nextNode; // To point to the next node
 
  // Position nodePtr at the head of the list.
  nodePtr = head;
